6fc82800ee665c1da142fcdd6a30ee6a0277edde
Xin <vincent7xin@gmail.com>
2019-04-22 15:31:37 +0800

    init

---
+++ src/main/java/com/github/ionething/sparrow/SparrowSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ionething.sparrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparrowSimpleApplication.class, args);
+	}
+
+}
